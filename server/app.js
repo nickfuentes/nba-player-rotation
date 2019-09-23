8ab471cd9449a7a8bdfafec36b066f2e532aed29
Nickolas Fuentes <nickfuentes24@gmail.com>
2019-09-23 12:40:55 -0500

    Able to add players to the database through add player route.

--- server/app.js
+++ server/app.js
@@ -6,19 +6,49 @@ const models = require("./models")
 app.use(cors())
 app.use(express.json())
 
+// Gets All The Players From The Database
 app.get("/", (req, res) => {
-  res.json("Index Route Working!")
+  models.Player.findAll({}).then(players => {
+    res.json(players)
+  })
 })
 
+// Adds Player To Database
 app.post("/add-player", (req, res) => {
   let firstName = req.body.firstName
+  let lastName = req.body.lastName
+  let teamName = req.body.teamName
+  let playerNumber = req.body.playerNumber
+  let position = req.body.position
+  let fg = req.body.fg
+  let threep = req.body.threep
+  let ft = req.body.ft
+  let reb = req.body.reb
+  let ast = req.body.ast
+  let blk = req.body.blk
+  let stl = req.body.stl
+  let pf = req.body.pf
+  let to = req.body.to
+  let pts = req.body.pts
+  let photoUrl = req.body.photoUrl
 
   let player = models.Player.build({
-    title: title,
-    genre: genre,
-    author: author,
-    year: year,
-    imageURL: imageURL
+    firstName: firstName,
+    lastName: lastName,
+    teamName: teamName,
+    playerNumber: playerNumber,
+    position: position,
+    fg: fg,
+    threep: threep,
+    ft: ft,
+    reb: reb,
+    ast: ast,
+    blk: blk,
+    stl: stl,
+    pf: pf,
+    to: to,
+    pts: pts,
+    photoUrl: photoUrl
   }).save()
 
   res.json({ Sucess: true })
